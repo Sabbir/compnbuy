48e5f2684b430b7a641a5b45aded7468fb127c56
Sabbir Hossain Tanvir <sabbirhs@yahoo.com>
2023-06-20 11:40:08 +0600

    con update

--- node-scrapper/src/controllers/productsController.js
+++ node-scrapper/src/controllers/productsController.js
@@ -14,6 +14,36 @@ const getAllProducts = (req, res) => {
     console.log(req.params.product)
     
 }
+const getAlleProducts = (req, res) => {
+   
+  product = req.params.product
+   
+   productsService.getAlleproducts(product).then((pr)=> {
+      //the promise is resolved here
+      
+      res.status(201).send(pr)
+    }).catch(err=>{
+      res.status(401).send({ error : err.message });
+    })
+    console.log(req.params.product)
+    
+}
+
+const getAllgProducts = (req, res) => {
+   
+  product = req.params.product
+   
+   productsService.getAllgproducts(product).then((pr)=> {
+      //the promise is resolved here
+      
+      res.status(201).send(pr)
+    }).catch(err=>{
+      res.status(401).send({ error : err.message });
+    })
+    console.log(req.params.product)
+    
+}
+
 const getAllProductsJSON = (req, res) => {
   const allProducts = productsService.getAllproductsJSON()
   res.status(201).send({ status: "OK", data: allProducts });
@@ -21,5 +51,7 @@ const getAllProductsJSON = (req, res) => {
 
 module.exports = {
                   getAllProducts,
+                  getAlleProducts,
+                  getAllgProducts,
                   getAllProductsJSON,
                 }  
