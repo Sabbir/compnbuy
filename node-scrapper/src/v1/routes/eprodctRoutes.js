04bbf8a7a433b92c6d4b46d0fb25bbafb8908d38
Sabbir Hossain Tanvir <sabbirhs@yahoo.com>
2023-06-20 11:38:26 +0600

    newU

---
+++ node-scrapper/src/v1/routes/eprodctRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const productsController = require("../../controllers/productsController")
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(400).send({status: "INFORMATION", data: "Please search for a product"});
+});
+
+router.get("/:product", productsController.getAlleProducts)
+router.get("/projson", productsController.getAllProductsJSON)
+
+module.exports = router
